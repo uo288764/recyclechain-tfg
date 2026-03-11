09871000f48959c143a6328d605f62112738281b
Carlos <uo288764@uniovi.es>
2026-03-11 22:40:36 +0100

    Sprint 3: Security

---
+++ backend/src/main/java/es/uniovi/recyclechain/backend/security/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package es.uniovi.recyclechain.backend.security;
+
+import es.uniovi.recyclechain.backend.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getIsActive();
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
